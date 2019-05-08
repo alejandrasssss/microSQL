cce1481c85f84a1a1e0dc9fb48db008647ccd1e2
alejandrasssss <aleesamayoa@gmail.com>
2019-05-08 16:43:45 -0600

    estructuras definidas para insertar y recibir del árbol

--- Proyecto_microSQL/Models/FabricaFila.cs
+++ Proyecto_microSQL/Models/FabricaFila.cs
@@ -7,5 +7,27 @@ namespace Proyecto_microSQL.Models
 {
     public class FabricaFila
     {
+        public Fila Fabricar(string textoTamañoFijo)
+        {
+            Fila fila = new Fila();
+            var datos = textoTamañoFijo.Split('-');  //cambiar caracter que los separa          
+            fila.ID = Convert.ToInt32(datos[0].Trim());
+            fila.INT1 = Convert.ToInt32(datos[1].Trim());
+            fila.INT2 = Convert.ToInt32(datos[2].Trim());
+            fila.INT3 = Convert.ToInt32(datos[3].Trim());
+            fila.DT1 = datos[4].Trim();
+            fila.DT2 = datos[5].Trim();
+            fila.DT3 = datos[6].Trim();
+            fila.VAR1 = datos[7].Trim();
+            fila.VAR2 = datos[8].Trim();
+            fila.VAR3 = datos[9].Trim();
+
+            return fila;
+        }
+
+        public Fila FabricarNulo()
+        {
+            return new Fila();
+        }
     }
 }
---
+++ Proyecto_microSQL/Models/Fila.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using ClassLibraryBTree;
+
+namespace Proyecto_microSQL.Models
+{
+    public class Fila : ITextoTamañoFijo
+    {
+        public int ID { get; set; }
+
+        public int INT1 { get; set; }
+        public int INT2 { get; set; }
+        public int INT3 { get; set; }
+
+        public string DT1 { get; set; }
+        public string DT2 { get; set; }
+        public string DT3 { get; set; }
+
+        public string VAR1 { get; set; }
+        public string VAR2 { get; set; }
+        public string VAR3 { get; set; }
+
+        private const string FormatoConst = "xxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxx-00-xxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxx";
+
+        public Fila()
+        {
+            ID = 0;
+            INT1 = 0;
+            INT2 = 0;
+            INT3 = 0;
+            DT1 = "";
+            DT2 = "";
+            DT3 = "";
+            VAR1 = "";
+            VAR2 = "";
+            VAR3 = "";
+        }
+
+        public Fila(int id, int int1, int int2, int int3, string dt1, string dt2, string dt3, string var1, string var2, string var3)
+        {
+            ID = id;
+            INT1 = int1;
+            INT2 = int2;
+            INT3 = int3;
+            DT1 = dt1;
+            DT2 = dt2;
+            DT3 = dt3;
+            VAR1 = var1;
+            VAR2 = var2;
+            VAR3 = var3;
+        }
+
+        public int FixedSizeText
+        {
+            get
+            {
+                return 81;
+            }
+        }
+
+        public override string ToString()
+        {
+            return ToFixedSizeString();
+        }
+
+        public string ToFixedSizeString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(ID.ToString().PadLeft(20, '0'));
+            //sb.Append('-');
+            //sb.Append(Apellido.PadLeft(20, 'x'));
+            //sb.Append('-');
+            //sb.Append(Edad.ToString().PadLeft(2, '0'));
+            //sb.Append('-');
+            //sb.Append(Username.PadLeft(15, 'x'));
+            //sb.Append('-');
+            //sb.Append(Password.PadLeft(20, 'x'));
+
+            return sb.ToString();
+        }
+    }
+}
